8e6b0cfd91bcaa062a3fb5bd4df26728943f5f69
abt1 <alex.todd2@virginmedia.com>
2016-08-30 13:20:08 +0100

    Changed 'Probability' to 'Frequency' & added total

--- YesNoBar.py
+++ YesNoBar.py
@@ -16,12 +16,15 @@ for row in data:
         for i in range(3):
             if int(choices[i]) > 1:
                 results[i] += 1
-results[:] = [x/datacount for x in results]
 fig = plt.figure(figsize=[20,14])
 ax = fig.add_subplot(1,1,1)
+for i in range(3):
+    plt.text(0.9,i+1,results[i],fontsize = 18)
+results[:] = [x/datacount for x in results]
+
 plt.barh((1,2,3),results, align='center', color='#2171b5', linewidth = 0, edgecolor = '#00008B')
 plt.yticks((1,2,3), answers, fontsize = 18)
-plt.xlabel('Probability', fontsize = 18)
+plt.xlabel('Frequency in data set', fontsize = 18)
 ax.set_title('How do the galaxy maps compare?', fontsize = 23)
 plt.xlim(0,1)
 plt.savefig(str(sys.argv[2]), bbox_inches = 'tight')
